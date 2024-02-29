d5156b37bf12a9cdd18e3a035f2976d01aa1ea2f
Mike Nguyen <mikenguyen7268@gmail.com>
2024-02-29 00:51:36 -0600

    modified GeneralMatching.h to use Blossom Algorithm.
    Blossom Algorithm has better execution time than the original randomized algorithm.

--- content/graph/GeneralMatching.h
+++ content/graph/GeneralMatching.h
@@ -1,54 +1,159 @@
 /**
- * Author: Simon Lindholm
- * Date: 2016-12-09
- * License: CC0
- * Source: http://www.mimuw.edu.pl/~mucha/pub/mucha_sankowski_focs04.pdf
- * Description: Matching for general graphs.
- * Fails with probability $N / mod$.
+ * Author: Monogon
+ * Date: 2021-06-28
+ * Source: https://codeforces.com/blog/entry/92339
+ * Description: Implementation of Blossom Algorithm for matching in general graphs.
+ * Usage: blossom b{num_vertices}; b.add_edge(i, j); ans = b.solve();
  * Time: O(N^3)
- * Status: not very well tested
+ * Status: tested by Monogon
  */
 #pragma once
 
-#include "../numerical/MatrixInverse-mod.h"
-
-vector<pii> generalMatching(int N, vector<pii>& ed) {
-	vector<vector<ll>> mat(N, vector<ll>(N)), A;
-	for (pii pa : ed) {
-		int a = pa.first, b = pa.second, r = rand() % mod;
-		mat[a][b] = r, mat[b][a] = (mod - r) % mod;
-	}
-
-	int r = matInv(A = mat), M = 2*N - r, fi, fj;
-	assert(r % 2 == 0);
-
-	if (M != N) do {
-		mat.resize(M, vector<ll>(M));
-		rep(i,0,N) {
-			mat[i].resize(M);
-			rep(j,N,M) {
-				int r = rand() % mod;
-				mat[i][j] = r, mat[j][i] = (mod - r) % mod;
-			}
-		}
-	} while (matInv(A = mat) != M);
-
-	vi has(M, 1); vector<pii> ret;
-	rep(it,0,M/2) {
-		rep(i,0,M) if (has[i])
-			rep(j,i+1,M) if (A[i][j] && mat[i][j]) {
-				fi = i; fj = j; goto done;
-		} assert(0); done:
-		if (fj < N) ret.emplace_back(fi, fj);
-		has[fi] = has[fj] = 0;
-		rep(sw,0,2) {
-			ll a = modpow(A[fi][fj], mod-2);
-			rep(i,0,M) if (has[i] && A[i][fj]) {
-				ll b = A[i][fj] * a % mod;
-				rep(j,0,M) A[i][j] = (A[i][j] - A[fi][j] * b) % mod;
-			}
-			swap(fi,fj);
-		}
-	}
-	return ret;
-}
+struct blossom {
+    int n, m;
+    vector<int> mate;
+    vector<vector<int>> b;
+    vector<int> p, d, bl;
+    vector<vector<int>> g;
+    blossom(int n) : n(n) {
+        m = n + n / 2;
+        mate.assign(n, -1);
+        b.resize(m);
+        p.resize(m);
+        d.resize(m);
+        bl.resize(m);
+        g.assign(m, vector<int>(m, -1));
+    }
+    void add_edge(int u, int v) {
+        g[u][v] = u;
+        g[v][u] = v;
+    }
+    void match(int u, int v) {
+        g[u][v] = g[v][u] = -1;
+        mate[u] = v;
+        mate[v] = u;
+    }
+    vector<int> trace(int x) {
+        vector<int> vx;
+        while (true) {
+            while (bl[x] != x)
+                x = bl[x];
+            if (!vx.empty() && vx.back() == x)
+                break;
+            vx.push_back(x);
+            x = p[x];
+        }
+        return vx;
+    }
+    void contract(int c, int x, int y, vector<int>& vx, vector<int>& vy) {
+        b[c].clear();
+        int r = vx.back();
+        while (!vx.empty() && !vy.empty() && vx.back() == vy.back()) {
+            r = vx.back();
+            vx.pop_back();
+            vy.pop_back();
+        }
+        b[c].push_back(r);
+        b[c].insert(b[c].end(), vx.rbegin(), vx.rend());
+        b[c].insert(b[c].end(), vy.begin(), vy.end());
+        for (int i = 0; i <= c; i++) {
+            g[c][i] = g[i][c] = -1;
+        }
+        for (int z : b[c]) {
+            bl[z] = c;
+            for (int i = 0; i < c; i++) {
+                if (g[z][i] != -1) {
+                    g[c][i] = z;
+                    g[i][c] = g[i][z];
+                }
+            }
+        }
+    }
+    vector<int> lift(vector<int>& vx) {
+        vector<int> A;
+        while (vx.size() >= 2) {
+            int z = vx.back();
+            vx.pop_back();
+            if (z < n) {
+                A.push_back(z);
+                continue;
+            }
+            int w = vx.back();
+            int i =
+                (A.size() % 2 == 0
+                     ? find(b[z].begin(), b[z].end(), g[z][w]) - b[z].begin()
+                     : 0);
+            int j = (A.size() % 2 == 1
+                         ? find(b[z].begin(), b[z].end(), g[z][A.back()]) -
+                               b[z].begin()
+                         : 0);
+            int k = b[z].size();
+            int dif = (A.size() % 2 == 0 ? i % 2 == 1 : j % 2 == 0) ? 1 : k - 1;
+            while (i != j) {
+                vx.push_back(b[z][i]);
+                i = (i + dif) % k;
+            }
+            vx.push_back(b[z][i]);
+        }
+        return A;
+    }
+    int solve() {
+        for (int ans = 0;; ans++) {
+            fill(d.begin(), d.end(), 0);
+            queue<int> Q;
+            for (int i = 0; i < m; i++)
+                bl[i] = i;
+            for (int i = 0; i < n; i++) {
+                if (mate[i] == -1) {
+                    Q.push(i);
+                    p[i] = i;
+                    d[i] = 1;
+                }
+            }
+            int c = n;
+            bool aug = false;
+            while (!Q.empty() && !aug) {
+                int x = Q.front();
+                Q.pop();
+                if (bl[x] != x)
+                    continue;
+                for (int y = 0; y < c; y++) {
+                    if (bl[y] == y && g[x][y] != -1) {
+                        if (d[y] == 0) {
+                            p[y] = x;
+                            d[y] = 2;
+                            p[mate[y]] = y;
+                            d[mate[y]] = 1;
+                            Q.push(mate[y]);
+                        } else if (d[y] == 1) {
+                            vector<int> vx = trace(x);
+                            vector<int> vy = trace(y);
+                            if (vx.back() == vy.back()) {
+                                contract(c, x, y, vx, vy);
+                                Q.push(c);
+                                p[c] = p[b[c][0]];
+                                d[c] = 1;
+                                c++;
+                            } else {
+                                aug = true;
+                                vx.insert(vx.begin(), y);
+                                vy.insert(vy.begin(), x);
+                                vector<int> A = lift(vx);
+                                vector<int> B = lift(vy);
+                                A.insert(A.end(), B.rbegin(), B.rend());
+                                for (int i = 0; i < (int)A.size(); i += 2) {
+                                    match(A[i], A[i + 1]);
+                                    if (i + 2 < (int)A.size())
+                                        add_edge(A[i + 1], A[i + 2]);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!aug)
+                return ans;
+        }
+    }
+};
